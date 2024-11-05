6804634972ed6be06de71acd2d44325c656ad738
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-11-05 20:34:57 +0100

    Feat: Done with updating of comment

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authenticateMiddleware from "../middleware/authentication";
-import { createComment, getComment } from "../controllers/comments";
+import { createComment, getComment, updateComment } from "../controllers/comments";
 
 const router: Router = Router()
 
 
-router.route('/:bookId/comments').post(authenticateMiddleware, createComment).get(getComment)
+router.route('/:bookId').post(authenticateMiddleware, createComment).get(getComment)
+router.route('/:bookId/:commentId').patch(authenticateMiddleware, updateComment)
 
 
 export default router
