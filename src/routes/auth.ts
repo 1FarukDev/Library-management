d11ecc5113926cd2d49b850ea150ada0f708c0a9
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-11-11 15:06:22 +0100

    Feat: Added the middleware to check if someone is veriied

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { register, login, verifyEmail } from "../controllers/auth";
+import checkVerified from "../middleware/check-verification";
 
 const router: Router = Router()
 
 router.post('/register', register)
-router.post('/login', login)
+router.post('/login', checkVerified, login)
 router.get("/verify-email", verifyEmail);
 
 export default router
