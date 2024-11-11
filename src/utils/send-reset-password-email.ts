9deeadbcab2c03783f7ab89295475401c1a890d9
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-11-11 23:56:15 +0100

    Feat: created a seperate file or the transporter

--- src/utils/send-reset-password-email.ts
+++ src/utils/send-reset-password-email.ts
@@ -1,15 +1,5 @@
-import nodemailer from "nodemailer";
-import dotenv from 'dotenv';
-dotenv.config();
+import transporter from "./transporter";
 
-const transporter = nodemailer.createTransport({
-    host: process.env.MAILTRAP_HOST,
-    port: 587,
-    auth: {
-        user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASS,
-    },
-});
 
 export const sendResetPasswordEmail = async (email: string, resetUrl: string) => {
     const mailOptions = {
