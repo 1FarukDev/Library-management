8a0c6f9d34d9340b5ab7667c171be91aa2508d41
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-11-25 10:58:21 +0100

    Feat: Done with the updating the user profile

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {  getUserProfile } from "../controllers/user";
+import { getUserProfile, updateUserProfile } from "../controllers/user";
+import authenticateMiddleware from "../middleware/authentication";
 
 const router: Router = Router()
 
 
 router.get("/user-profile", getUserProfile)
+router.patch("/user-profile", authenticateMiddleware, updateUserProfile)
 
 export default router
